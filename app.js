43fc004260b3ee8d745f3a0e8c98a1e40dcefab4
Matt Pedersen <mpedersen2054@gmail.com>
2014-11-17 23:32:36 -0600

    add complete code for refactored fizzbuzz app

---
+++ app.js
@@ -0,0 +1,47 @@
+
+$(function() {
+  var endNum = promptUser(),
+      body = $("body");
+
+  for (var i = 1; i < endNum+1; i++) {
+
+    if (i % 5 == 0 && i % 3 == 0) {
+      body.append("FizzBuzz");
+      newLine();
+    }
+    else if (i % 5 == 0) {
+      body.append("Buzz");
+      newLine();
+    }
+    else if (i % 3 == 0) {
+      body.append("Fizz");
+      newLine();
+    }
+    else {
+      body.append(i);
+      newLine();
+    }
+
+  }
+
+  function newLine() {
+    body.append("<hr />");
+  }
+
+  function promptUser() {
+    var res = prompt("Enter a value and see this app count to the desired value while replacing values divisible by 3 with 'Fizz', and 5 with 'Buzz'"),
+        resNum = +res;
+
+    if (typeof resNum != "number") {
+      return prompt("Please enter a number.");
+    }
+    else if (resNum > 100) {
+      var newNum = prompt("Please enter a number smaller than 100.");
+      return +newNum;
+    }
+    else {
+      return resNum;
+    }
+  }
+
+})();
